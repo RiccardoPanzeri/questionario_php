78731d06e6c136995c2e535cfae0f3c9dab2e9ea
RiccardoPanzeri <riccardo.panzeri@outlook.it>
2024-10-08 05:51:29 +0200

    modifiche minori allo script

--- pages/result.php
+++ pages/result.php
@@ -7,18 +7,21 @@
 </head>
 <body>
     <?php
+    //do inizio alla sessione
     session_start();
+    //controllo che i valori siano stati correttmente conservati nella variabile superglobale
     if(isset($_SESSION["rightAnswers"]) && isset($_SESSION["userAnswers"])){
         $answerCount = 1;
         $rightAnswers = $_SESSION["rightAnswers"];
         $userAnswers = $_SESSION["userAnswers"];
-    }else{
+    }else{//altrimenti, reindirizzo l'utente alla home e fermo lo script
         header("Location:../index.html");
         exit(1);
     }
-
+    //mostro il punteggio all'utente
     echo "<h1 class= text>Il tuo Punteggio è: {$_SESSION['score']} su {$_SESSION['totalAnswers']}</h1>";
     echo "<h2 class= text>Ecco la soluzione:</h2>";
+    //mostro le crisposte corrette all'utente, con eventuale risposta sbagliata fornita
     foreach($rightAnswers as $answer => $value){
         if($userAnswers[$answer] === $rightAnswers[$answer]){
             echo "<p class=para> Risposta corretta alla domanda numero {$answerCount}: <span class='rightAnswer'>{$rightAnswers[$answer]}</span>. Corretto!</p>";
@@ -26,7 +29,7 @@
         }else{
             echo "<p class=para> Risposta corretta alla domanda numero {$answerCount}: <span class='rightAnswer'>{$rightAnswers[$answer]}</span>.<br> Hai risposto: <span class='wrongAnswer'>{$userAnswers[$answer]}</span></p>";
         }
-        $answerCount++;
+        $answerCount++;//aumento il counter delle risposte
     }
 
     echo "<a href='../index.html'>Torna alla HomePage</a>";
--- pages/score_handler.php
+++ pages/score_handler.php
@@ -1,30 +1,31 @@
 <?php 
 session_start();
 $score = 0;
+//array con le risposte corrette
 $rightAnswers = [
     "answer1" => "PHP",
     "answer2" => "Il Browser",
     "answer3" => "Un Array Associativo"
 ];
-$userAnswers = [];
+$userAnswers = [];//array che
 $totalAnswers = count($rightAnswers);
 
-
+//controllo che il metodo sia post e che i valori forniti con il form non siano vuoti 
 if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["answer1"]) && 
 isset($_POST["answer2"]) && isset($_POST["answer3"])){
-    foreach($rightAnswers as $answerNum => $value){
-        if(htmlspecialchars($_POST[$answerNum]) === $rightAnswers[$answerNum]){
-            $score++;
+    foreach($rightAnswers as $answerNum => $value){//per ogni valore contenuto nell'array delle risposte corrette
+        if(htmlspecialchars($_POST[$answerNum]) === $rightAnswers[$answerNum]){//se la risposta fornita e quella corretta coincidono, aumento il punteggio
+            $score++;  
         }
-        $userAnswers[$answerNum] = htmlspecialchars($_POST[$answerNum]);
+        $userAnswers[$answerNum] = htmlspecialchars($_POST[$answerNum]);//conservo la risposta dell'utente in un array apposito, che mi servirà dopo
     }
 
-}else{
+}else{//se il metodo non è post o se i campi sono vuoti interrompo lo script e reindirizzo l'utente
     header("Location: ../index.html");
     exit(1);
 
 }
-
+//conservo le risposte dell'utente, le risposte corrette, il totale delle domande e il punteggio calcolato nella superglobale di sessione
 $_SESSION["score"] = $score;
 $_SESSION["rightAnswers"] = $rightAnswers;
 $_SESSION["userAnswers"] = $userAnswers;
